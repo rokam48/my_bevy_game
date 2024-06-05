ececc3e2993db2548ad03b766d922de5062dc8c5
Rohit Kamble <rotusmailbox@gmail.com>
2024-06-05 17:34:45 +0530

    Basic Bevy ECS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello, Bevy!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Albert Einstein".to_string())));
+    commands.spawn((Person, Name("Bilbo Baggins".to_string())));
+    commands.spawn((Person, Name("Cillian Murphy".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Cillian Murphy" {
+            name.0 = "Christian Bale".to_string();
+            break;
+        }
+    }
 }
