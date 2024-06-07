e5b081e32dab577783f9b36dfb43872d696a4751
Rohit Kamble <rotusmailbox@gmail.com>
2024-06-07 21:23:32 +0530

    add run animation for a sprite

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,45 @@
 use bevy::prelude::*;
-use hello::HelloPlugin;
-
-pub mod hello;
 
 pub fn run_game() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
+        .run();
+}
+
+#[derive(Component)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("../assets/_Run.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(120.0, 80.0), 10, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                ..default()
+            },
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+}
+
+fn animate_sprite(time: Res<Time>, mut query: Query<(&mut AnimationTimer, &mut TextureAtlas)>) {
+    for (mut timer, mut atlas) in &mut query {
+        timer.0.tick(time.delta());
+        if timer.0.just_finished() {
+            atlas.index = if atlas.index == 9 { 0 } else { atlas.index + 1 };
+        }
+    }
 }
